1aceb775add5187a2faabe16a8b3f3759264d15e
Michal Kuracina <michal.kuracina@henkel.com>
2022-07-29 15:38:24 +0200

    Get status, upload main thread

--- TimeKeeper/Form1.Designer.cs
+++ TimeKeeper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TimeKeeper
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.Location = new System.Drawing.Point(50, 15);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(100, 30);
+            this.lbl_Status.TabIndex = 0;
+            this.lbl_Status.Text = "label1";
+            this.lbl_Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(184, 61);
+            this.Controls.Add(this.lbl_Status);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeKeeper";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Status;
     }
 }
--- TimeKeeper/Form1.cs
+++ TimeKeeper/Form1.cs
@@ -7,14 +7,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Automation;
 
 namespace TimeKeeper
 {
-    public partial class Form1 : Form
+    public partial class form1 : Form
     {
-        public Form1()
+        private BackgroundWorker _backgroundWorker;
+        private bool ElementFound { get; set; }
+
+        public form1()
         {
             InitializeComponent();
+
+            // Set windows position
+
+            //this.StartPosition = FormStartPosition.Manual;
+            //this.Location = new Point(100, Screen.PrimaryScreen.Bounds.Height - 200);
+
+            // Initialize BackgroundWorker
+
+            _backgroundWorker = new BackgroundWorker()
+            {
+                WorkerReportsProgress = true,
+            };
+
+            _backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            _backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Condition of ToolBar
+            AndCondition condition = new AndCondition(
+                new PropertyCondition(AutomationElement.ClassNameProperty, "MSTaskListWClass"),
+                new PropertyCondition(AutomationElement.FrameworkIdProperty, "Win32"),
+                new PropertyCondition(AutomationElement.LocalizedControlTypeProperty, "tool bar"),
+                new PropertyCondition(AutomationElement.NameProperty, "Running applications"));
+
+            // First toolbar element
+            AutomationElement toolBarElement = AutomationElement.RootElement.FindFirst(TreeScope.Descendants, condition);
+
+            // Condition of all 2 types which can be in the ToolBar
+            OrCondition condition1 = new OrCondition(
+                new PropertyCondition(AutomationElement.LocalizedControlTypeProperty, "button"),
+                new PropertyCondition(AutomationElement.LocalizedControlTypeProperty, "menu item"));
+
+            // FindAll buttons and menu items
+            AutomationElementCollection elements = toolBarElement.FindAll(TreeScope.Children, condition1);
+
+            ElementFound = false;
+            AutomationElement myElement = null;
+
+            foreach (AutomationElement el in elements)
+            {
+                string name = el.Current.Name;
+                if (name.Contains("Microsoft Teams"))
+                {
+                    ElementFound = true;
+                    myElement = el;
+                    break;
+                }
+            }
+
+            // Terminate the app if Microsoft Teams is not existing
+
+            if (!ElementFound)
+            {
+                throw new ElementNotAvailableException("Microsoft Teams must be pinned to your taskbar.");
+            }
+
+            // Check for 'HelpText' property of the element in infitite loop
+            // This is becasue elements in taskbar are not emitting any events except focus
+            // Also 'AutomationPropertyChangedEventHandler' is not working here :/
+
+            while (true)
+            {
+                // Status set to Be right back
+                // Status set to Away
+                // Status set to Offline
+                // property does not exists
+                try
+                {
+                    bool notStarted = myElement.Current.HelpText.Contains("");
+                    bool beRigthBack = myElement.Current.HelpText.Contains("Status set to Be right back");
+                    bool away = myElement.Current.HelpText.Contains("Status set to Away");
+                    bool offline = myElement.Current.HelpText.Contains("Status set to Offline");
+                    if (notStarted != true | beRigthBack != false | away != false | offline != false)
+                    {
+                        _backgroundWorker.ReportProgress(1, myElement.Current.HelpText); // Send information to main thread
+                    }
+                }
+                catch (Exception)
+                {
+                    // 'HelpText' property does not exist because Microsoft Teams is not started
+                }
+
+                
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string fromWorker = e.UserState.ToString();
+            lbl_Status.Text = fromWorker;
         }
 
     }
